531bc1510f7a5ce7e71d40c42fb491904999b589
Ridhsuki <basukiridhoalghifary@gmail.com>
2025-10-11 00:56:17 +0700

    chore: restyling badal page

--- app/Models/Badal.php
+++ app/Models/Badal.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Badal extends Model
 {
     protected $fillable = ['service_id', 'name', 'price'];
-    public function service(){
+    public function service()
+    {
         return $this->belongsTo(Service::class);
     }
 }
