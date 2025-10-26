05a597d51bc7cc57aae03285e91488fe9c34ae81
1mGoodDev <sulthon.elmanaar@gmail.com>
2025-10-26 17:16:09 +0700

    modifikasi sedikit untuk membuat ukuran class dinamis

--- resources/views/components/card-component.blade.php
+++ resources/views/components/card-component.blade.php
@@ -1,4 +1,5 @@
-<div class="col-xl-3 col-md-6" id="card-reponsive">
+{{-- Hapus col-* dari sini, tambahkan $attributes->merge --}}
+<div {{ $attributes->merge(['class' => '']) }} id="card-reponsive">
     <div class="card card-stat h-100">
         <div class="card-body">
             <div class="d-flex justify-content-between align-items-center">
@@ -6,13 +7,14 @@
                     <h6 class="card-title mb-3">{{ $title }}</h6>
                     <h3 class="card-subtitle fw-bold mb-2" style="color: var(--haramain-primary);">
                         {{ $count }}
-                    </h3>
-                    <p class="card-text {{ $textColor }} mb-0"><small>{{ $desc }}</small></p>
-                </div>
+                        </h3>
+                    <p class="card-text {{ $textColor }} mb-0"><small>{{ $desc }}</small>
+                    </p>
+                    </div>
                 <div class="bg-opacity-10 p-3 rounded">
                     <i class="bi {{ $icon }} fs-2" style="color: {{ $iconColor }};"></i>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 </div>
